c8b3b6746d869893bcc1c1ff69273b3f27b0e897
ultraslayyy <angusbrowning.switch@gmail.com>
2025-04-01 08:25:38 +1100

    Update schema.ts
    Added support for slugs, for URLs and easily loading tournament pages through like a routes/tournament/[slug]/+page.svelte and +page.ts.
    Also added a logo index which is just a regular ol string, but longer just in case the url is really long or something

--- Tournaments/src/db/schema.ts
+++ Tournaments/src/db/schema.ts
@@ -4,7 +4,9 @@ import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 export const tournaments = pgTable("tournaments", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).notNull(),
+    slug: varchar("slug", { length: 255 }).notNull().check(`slug ~ '^[a-z0-9_-]+$'`),
     description: varchar({ length: 255 }).notNull(),
+    logo: varchar({ length: 500 }).notNull(),
 });
 
 export const pools = pgTable("pools", {
