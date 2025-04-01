9b9da00dc4be1dbfe0ca8efd397729f7dc04ba62
ultraslayyy <ultraslay@pbui.net>
2025-04-01 15:05:23 +1100

    Fixed slug (made unique and fixed the CHECK)

--- Tournaments/src/db/schema.ts
+++ Tournaments/src/db/schema.ts
@@ -1,13 +1,16 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { integer, pgTable, varchar, check } from "drizzle-orm/pg-core";
 
 
 export const tournaments = pgTable("tournaments", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).notNull(),
-    slug: varchar("slug", { length: 255 }).notNull().check(`slug ~ '^[a-z0-9_-]+$'`),
+    slug: varchar("slug", { length: 255 }).unique().notNull(),
     description: varchar({ length: 255 }).notNull(),
     logo: varchar({ length: 500 }).notNull(),
-});
+}, (table) => [
+    check('slug_check', sql`${table.slug} ~ '^[a-z0-9_-]+$'`),
+]);
 
 export const pools = pgTable("pools", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
