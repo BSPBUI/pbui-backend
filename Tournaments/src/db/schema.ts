ff018fda6f9ba8e452efd2fd823ee81433cd1f38
Angus-Developer <angusbrowning.switch@gmail.com>
2025-04-01 18:19:13 +1100

    Add flow and pools array
    Pools array is going to be an array of the pool ids (from "pools" pgTable)
    Added checks for flow to make sure that flow is structure correctly in the backend:
        "flow": [
            { "player": [integer of 1, 2, or 0], "action": [string of 'pick', or 'ban' (setting player as 0 means tiebreaker)] },
            ...etc
        ]

--- Tournaments/src/db/schema.ts
+++ Tournaments/src/db/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { integer, pgTable, varchar, check } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, check, jsonb } from "drizzle-orm/pg-core";
 
 
 export const tournaments = pgTable("tournaments", {
@@ -8,8 +8,19 @@ export const tournaments = pgTable("tournaments", {
     slug: varchar("slug", { length: 255 }).unique().notNull(),
     description: varchar({ length: 255 }).notNull(),
     logo: varchar({ length: 500 }).notNull(),
+    pools: integer().array().notNull(),
+    flow: jsonb("flow").notNull()
 }, (table) => [
     check('slug_check', sql`${table.slug} ~ '^[a-z0-9_-]+$'`),
+    check('flow_check', sql`
+        jsonb_typeof(${table.flow}) = 'array' AND
+        NOT EXISTS (
+            SELECT * FROM jsonb_array_elements(${table.flow}) AS elem
+            WHERE
+                (elem->>'player')::int NOT IN (0, 1, 2) OR
+                (elem->>'action') NOT IN ('pick', 'ban')
+        )
+    `)
 ]);
 
 export const pools = pgTable("pools", {
